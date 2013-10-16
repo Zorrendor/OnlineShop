128b4a30f77bd041b9483141f62fab57a6c829ea
Zorrendor <zorrendor@gmail.com>
2013-10-16 12:49:13 +0300

    Заливаю первые наработки

---
+++ OnlineShop/Models/Furniture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class Furniture
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        float Price { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
+        int Length { get; set; }
+        string Photo { get; set; }
+        string TypeFurniture { get; set; }
+    }
+}
---
+++ OnlineShop/Models/FurnitureContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OnlineShop.Models
+{
+    public class FurnitureContext : DbContext
+    {
+        public DbSet<Furniture> Furnitures { get; set; }
+    }
+}
